5388318f0d6fee6054bf86c292f5b67d3221d700
YamineTamourt <102610049+YamineTamourt@users.noreply.github.com>
2022-05-23 09:36:15 +0200

    Update Init.sh

--- Init.sh
+++ Init.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
+echo "Entrer le lien de la playlist/video (vide pour generer la playlist d'exemple)"
+echo ""
+
+read lien
+
+if [[ -z $lien ]]
+then
+	echo ""
+	echo "generation de la playlist d'exemple ..."
+	echo ""
+	
+	systemctl start jellyfin
+
+	exit 0
+fi
+
+youtube-dl -s $lien > /dev/null 2> /dev/null
+
+if [ $? -ne 0 ]
+then
+	echo ""
+	echo "lien invalide"
+	echo "generation de la playlist d'exemple ..."
+	echo ""
+else
+	echo ""
+	echo "telechargement de la playlist/video ..."
+	echo ""
+	
+	cd /media
+  
+  rm *
+
+	youtube-dl $lien
+fi
+
 systemctl start jellyfin
+
+exit 0
